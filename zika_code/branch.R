8c4147dce731ad9bd158779c4cd5bd1c1e0a7610
vagrant <spencer.fox85@gmail.com>
2016-03-21 13:50:28 -0500

    initialize the branching code. First simple models

---
+++ zika_code/branch.R
@@ -0,0 +1,96 @@
+
+# Three kinds of counters:
+# UI - Undiscovered Infecteds
+# DI - Discovered Infecteds
+# D - Cumulative number of discoveries
+#
+# Simulate until we have either:
+# - we run out of infecteds
+# - e_thresh number of infecteds, and d_thresh number of discoveries
+run_branch <- function(prop_p, recov_p, disc_p, d_thres, e_thresh) {
+  UI = 1; DI = 0; D = 0
+  I = UI + DI
+  time_record <- data.frame(matrix(data = c(I,D), ncol = 2))
+  colnames(time_record) <- c("I", "D")
+  
+  while  (((I < e_thresh) & (I > 0)) | ((I > 0) & (D < d_thresh))) {
+    #while Number of infected is below epidemic threshold hold and more than 0 infected
+    # or while number of infecteds is above 0 and the number of detected is below threshold
+    
+    newDI_draws = runif(DI) #New discovered infected possibilities 
+    newDI_count = sum(newDI_draws < prop_p) #Number of new infected by detected individuals 
+    
+    decDI_draws = runif(DI) #Detected indivduals-will they recover?
+    decDI_count = sum(decDI_draws < recov_p)
+    
+    newUI_draws = runif(UI) #Number of possible new Undiscovred infecteds 
+    newUI_count =  sum(newUI_draws < prop_p)  #Number of new Infecteds 
+    
+    decUI_draws = runif(UI) #Undiscovered infecteds that stay infected
+    decUI_count = sum(decUI_draws < recov_p)
+    
+    disUI_draws = runif(UI)
+    disUI_count = sum(disUI_draws < disc_p)  #probability that undetected individuals are discovered 
+    
+    removeUI = sum((decUI_draws < recov_p) | (disUI_draws < disc_p)) 
+
+    #Undiscovered infecteds can remove by being recovered or by being discovoered 
+    
+    removeUDI =  sum((decUI_draws < recov_p) & ( disUI_draws < disc_p))
+  
+    #remove undiscovered infecteds that are both no longer infected and were eventually discovered 
+    
+    UI = UI + newUI_count + newDI_count - removeUI 
+    #Undiscovered = previously undiscovered + newly infected by UI + newly infected by DI - either recovered or discovered 
+    DI = DI - decDI_count + disUI_count - removeUDI
+    #Detected = Detected + Recovered Individuals + Newly Detected - Previously UI that have been detected and Recovered 
+    D = D + disUI_count #Cumulatve Detected = those detected + newly detected
+    I = UI + DI #Undiscovered and Discovered Infected 
+    time_record <- rbind(time_record,c(I,D))
+  }
+  return(time_record)
+}
+
+
+# Estimate the probability we will reach e_thresh number of I's (as opposed to
+# the infection dying out) given we have discovered at least d_thresh cases
+#
+# prop_p -- probability an I infects a new individual in a time period
+# recov_p -- probability an I recovers in a time period
+# disc_p -- probability we discover an I
+# d_thresh -- unused... number of discoveries
+# e_thresh -- total instantaneous I's that count as epidemic escape (not cumulative)
+
+prob_ext <- function(prop_p, recov_p, disc_p, d_thresh, e_thresh, nsamples=10000) {
+  escapes = 0
+  i = 1 
+  while (i < nsamples) {
+    record = run_branch(prop_p, recov_p, disc_p, d_thresh, e_thresh) #Run the simulation 
+    Final.I = record[nrow(record),1]
+    # print(paste("Final.I", Final.I, sep = "-"))
+    Final.D = record[length(record),2]
+    # print(paste("Final.D", Final.D, sep = "-"))
+    #if (Final.D < d_thresh){  
+    if (Final.I > e_thresh) {
+      escapes = escapes + 1
+    }
+    i = i+1
+    print(paste('Estimate', escapes / i, sep = ": "))
+    # }    
+  }
+}
+
+# prop_p -- probability an I infects a new individual in a time period: how to determine this? 
+# recov_p -- probability an I recovers in a time period: human recovery 
+# disc_p -- probability we discover an I: symptom ratio 
+# d_thresh -- unused... number of discoveries
+# e_thresh -- total instantaneous I's that count as epidemic escape (not cumulative):
+
+prop_p <- 2/7
+recov_p <- 1.0/7
+disc_p <- .01
+d_thresh <- 5
+e_thresh <- 150
+
+prob_ext(prop_p, recov_p, disc_p, d_thresh, e_thresh)
+  
---
+++ zika_code/branch.py
@@ -0,0 +1,71 @@
+import scipy
+
+# Three kinds of counters:
+# UI - Undiscovered Infecteds
+# DI - Discovered Infecteds
+# D - Cumulative number of discoveries
+#
+# Simulate until we have either:
+# - we run out of infecteds
+# - e_thresh number of infecteds, and d_thresh number of discoveries
+#
+# prop_p -- probability an I infects a new individual in a time period
+# recov_p -- probability an I recovers in a time period
+# disc_p -- probability we discover an I
+# d_thresh -- unused... number of discoveries
+# e_thresh -- total instantaneous I's that count as epidemic escape (not cumulative)
+def run_branch(prop_p, recov_p, disc_p, d_thresh, e_thresh):
+    (UI,DI,D) = 1,0,0
+    I = UI + DI
+    time_record = [(I,D)]
+    while ( (I < e_thresh) and (I > 0) ) or ( (I > 0) and (D < d_thresh) ):
+        newDI_draws = scipy.rand(DI)
+        newDI_count = scipy.sum( newDI_draws < prop_p) 
+        decDI_draws = scipy.rand(DI)
+        decDI_count = scipy.sum( decDI_draws < recov_p) 
+
+        newUI_draws = scipy.rand(UI)
+        newUI_count = scipy.sum( newUI_draws < prop_p) 
+        decUI_draws = scipy.rand(UI)
+        decUI_count = scipy.sum( decUI_draws < recov_p) 
+        disUI_draws = scipy.rand(UI)
+        disUI_count = scipy.sum( disUI_draws < disc_p) 
+
+        removeUI = scipy.sum( (decUI_draws < recov_p) | ( disUI_draws < disc_p) )
+        removeUDI = scipy.sum( (decUI_draws < recov_p) & ( disUI_draws < disc_p) )
+
+        UI = UI + newUI_count + newDI_count - removeUI
+        DI = DI - decDI_count + disUI_count - removeUDI
+        D = D + disUI_count
+        I = UI + DI
+        time_record.append( (I,D) )
+    return time_record
+
+# Estimate the probability we will reach e_thresh number of I's (as opposed to
+# the infection dying out) given we have discovered at least d_thresh cases
+#
+# prop_p -- probability an I infects a new individual in a time period
+# recov_p -- probability an I recovers in a time period
+# disc_p -- probability we discover an I
+# d_thresh -- unused... number of discoveries
+# e_thresh -- total instantaneous I's that count as epidemic escape (not cumulative)
+def prob_ext(prop_p, recov_p, disc_p, d_thresh, e_thresh, nsamples=10000):
+    escapes = 0
+    i = 0
+    while i < nsamples:
+        record = run_branch(prop_p, recov_p, disc_p, d_thresh, e_thresh)
+        I,D = record[-1]
+        if D < d_thresh:
+            continue
+        if I > e_thresh:
+            escapes += 1
+        i += 1 
+        print 'Estimate', float(escapes) / i
+
+# R0 is prop_p / recov_p (propagation probability / recovery probability)
+# disc_p is probability we discover a case
+# disc_threshold is how many cases we have "discovered"
+# and e_threshold is the escape number
+# estimates the probability escape will happen, if we have discovered at least d_thresh cases
+prob_ext(2/7, 1.0/7, .99, 5, 150)
+
