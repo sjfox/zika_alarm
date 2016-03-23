873c397ae0bfb57959f3cff2b6ac788ff758d944
Spencer Fox <spencer.fox85@gmail.com>
2016-03-23 16:30:52 -0500

    added simple

--- zika_code/sf_script.R
+++ zika_code/sf_script.R
@@ -15,7 +15,7 @@ library(cowplot)
 # e_thresh -- total instantaneous I's that count as epidemic escape (not cumulative):
 
 #Parameters 
-branch_params <- function(prop_p = .7/7 , 
+branch_params <- function(prop_p = 1/7 , 
                           recov_p = 1.0/7,
                           d_thres = 5,
                           e_thresh = 300,
@@ -23,5 +23,30 @@ branch_params <- function(prop_p = .7/7 ,
                           incub_p = 1/16.5,
                           dis_prob_symp = 1,
                           dis_prob_asymp = 0.00 ,
-                          intro_rate = 0.000)
+                          intro_rate = 0.000,
+                          disc_p = .1)
   return(as.list(environment()))
+
+run_simple_branches <- function(num_reps, ...){
+  rlply(.n = num_reps, .expr = run_branch_simple(...) )
+}
+
+getMaxCumI <- function(x){
+  return(x$cumI[nrow(x)])
+}
+all_getMaxCumI <- function(x) {
+  laply(x, getMaxCumI)
+}
+trials <- run_simple_branches(1000, prop_p=.5/7, recov_p=1/7, disc_p=.01, d_thresh=5, e_thresh=200)
+final.sizes <- all_getMaxCumI(trials)
+hist(final.sizes, breaks=100)
+
+
+
+
+
+trials <- run_branches(num_reps = 1000, branch_params(prop_p=.5/7, dis_prob_symp = .01))
+p <- plot_final_sizes(trials)
+print(p)
+
+ggsave(filename = "../ExploratoryFigures/r0_0.2_disc_0.01_hist.pdf", plot = p, width=6, height=5)
