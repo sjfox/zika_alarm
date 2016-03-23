1a6fcf0243ccae994a3663fe320cdddc9b91c305
Spencer Fox <spencer.fox85@gmail.com>
2016-03-23 11:38:05 -0500

    changed to not print anything but plot

--- zika_code/plot.functions.R
+++ zika_code/plot.functions.R
@@ -5,7 +5,6 @@
 
 plot_final_sizes <- function(x){
   final.sizes <- all_last_cuminfect_values(x)
-  print(final.sizes)
   qplot(final.sizes, ..density.., geom="histogram", binwidth=5) +
     scale_y_continuous(expand=c(0,0)) + 
     scale_x_continuous(expand=c(0,0))+
