77f5d38f95e9ce97d30c19b17c9d86468044a639
Spencer Fox <spencer.fox85@gmail.com>
2016-04-05 12:41:20 -0500

    Set up code for TACC

---
+++ zika_code/create_run_branches_script.R
@@ -0,0 +1,27 @@
+rm(list=ls())
+if(grepl('spencerfox', Sys.info()['login'])) setwd('~/projects/zika_alarm/zika_code/')
+if(grepl('vagrant', Sys.info()['user'])) setwd('/vagrant/zika_alarm/zika_code/')
+if(grepl('sjf826', Sys.info()['login'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('tacc', Sys.info()['nodename'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('meyerslab', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+if(grepl('laurencastro', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+
+
+r_nots <- seq(0.8, 2, by=0.05)
+disc_probs <- c(0.01, .1)
+intro_rates <- c(0.0, 0.001, .01, .1)
+
+sink('../launcher/run_branches.txt')
+for(r_not in r_nots){
+  for(disc_prob in disc_probs){
+    for(intro_rate in intro_rates){
+      startCmd <- "R CMD BATCH '--no-restore --no-save --args"
+      paramCmd <- paste0(' desired_Rnot=', r_not, ' disc_prob=', disc_prob, ' intro_rate=', intro_rate, " '")
+      endCmd <- " ../zika_code/run_branches.R"
+      full_cmd <- paste0(startCmd, paramCmd, endCmd)
+      cat(full_cmd)              
+      cat('\n')              
+    }
+  }
+}
+sink()  
---
+++ zika_code/run_branches.R
@@ -0,0 +1,40 @@
+rm(list=ls())
+if(grepl('spencerfox', Sys.info()['login'])) setwd('~/projects/zika_alarm/zika_code/')
+if(grepl('vagrant', Sys.info()['user'])) setwd('/vagrant/zika_alarm/zika_code/')
+if(grepl('sjf826', Sys.info()['login'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('tacc', Sys.info()['nodename'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('meyerslab', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+if(grepl('laurencastro', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+
+
+args <- (commandArgs(TRUE)) ## load arguments from R CMD BATCH
+
+## Needs 
+if(length(args)>0)  { ## Then cycle through each element of the list and evaluate the expressions.
+  print(paste0('loading in ', args, ' from R CMD BATCH'))
+  for(i in 1:length(args)) {
+    eval(parse(text=args[[i]]))
+  }
+}
+
+source("incubation_branch.R")
+
+branch_params <- function(r_not = 1.1,
+                          infBoxes = 6,
+                          incBoxes = 6,
+                          recov_p = 0.6071267,
+                          incub_rate = 0.5777811,
+                          prop_p =  r_not*recov_p/infBoxes, 
+                          d_thres = 5,
+                          e_thresh = 500,
+                          prob_symp = 1,
+                          dis_prob_symp = .01,
+                          dis_prob_asymp = 0.00 ,
+                          intro_rate = 0.000)
+return(as.list(environment()))
+
+params <- branch_params(r_not=desired_Rnot, dis_prob_symp = disc_prob, intro_rate = intro_rate )
+
+trials <- run_branches_inc(num_reps = 10000, params)
+
+save(list = c("trials", "params"), file = paste0("../../workfolder/data/zika/", "zika_sims_", desired_Rnot, "_", disc_prob, "_", intro_rate, ".Rdata"))
