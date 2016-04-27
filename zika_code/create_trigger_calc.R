a1e64c5255f32257e5f87acb97b720daff59bf59
vagrant <spencer.fox85@gmail.com>
2016-04-27 16:17:30 -0500

    making trigger calcs parallelized on tacc

---
+++ zika_code/create_trigger_calc.R
@@ -0,0 +1,22 @@
+rm(list=ls())
+if(grepl('spencerfox', Sys.info()['login'])) setwd('~/projects/zika_alarm/zika_code/')
+if(grepl('vagrant', Sys.info()['user'])) setwd('/vagrant/zika_alarm/zika_code/')
+if(grepl('sjf826', Sys.info()['login'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('tacc', Sys.info()['nodename'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('meyerslab', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+if(grepl('laurencastro', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+
+
+dirPath <- "../../workfolder/data/zika_all/"
+
+data.files <- list.files(path=dirPath, pattern="*.Rdata", full.names=T, recursive=FALSE)
+sink('trigger_calc.txt')
+for(file in data.files){
+  startCmd <- "R CMD BATCH '--no-restore --no-save --args"
+  fileCmd <- paste0(' data.file="', file, '"')
+  endCmd <- "' ../zika_code/trigger_calc.R"
+  full_cmd <- paste0(startCmd, fileCmd, endCmd)
+  # print(full_cmd)
+  cat(full_cmd)               # add command
+  cat('\n')              # add new line
+}
---
+++ zika_code/trigger_calc.R
@@ -0,0 +1,37 @@
+rm(list=ls())
+if(grepl('spencerfox', Sys.info()['login'])) setwd('~/projects/zika_alarm/zika_code/')
+if(grepl('vagrant', Sys.info()['user'])) setwd('/vagrant/zika_alarm/zika_code/')
+if(grepl('sjf826', Sys.info()['login'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('tacc', Sys.info()['nodename'])) setwd('/home1/02958/sjf826/zika_alarm/zika_code/')
+if(grepl('meyerslab', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+if(grepl('laurencastro', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zika_code/')
+
+library(plyr)
+args <- (commandArgs(TRUE)) ## load arguments from R CMD BATCH
+
+## Needs 
+if(length(args)>0)  { ## Then cycle through each element of the list and evaluate the expressions.
+  print(paste0('loading in ', args, ' from R CMD BATCH'))
+  for(i in 1:length(args)) {
+    eval(parse(text=args[[i]]))
+  }
+}
+
+sapply(c('branch.functions.R','plot.functions.R', 'incubation_branch.R', 'analyze_saved_sims.R'), source)
+
+prevalences <- c(15,20,25)
+confidences <- seq(0.5, 0.95, by=0.05)
+
+load(data.file)
+params <- get_parms(data.file)
+triggers <- data.frame()
+for(prevalence in prevalences){
+  for(conf in confidences){
+    epi <- get_epidemic_trigger(trials = trials, threshold = prevalence, confidence = conf)  
+    prev <- get_surveillance_trigger(trials = trials,threshold =  prevalence, confidence = conf)  
+    triggers <- rbind(triggers, cbind(params, data.frame(threshold=prevalence, confidence=conf, epi_trigger=epi, prev_trigger=prev)))
+  }
+}
+
+save(list = c("triggers"), file = paste0("../../workfolder/data/zika_triggers_all/", "zika_triggers_", params$r_not, "_", params$disc_prob, "_", params$intro_rate, ".Rdata"))
+
