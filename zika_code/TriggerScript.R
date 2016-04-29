b87d3cfa5373da429998b7f591abd63ff0bbd412
Spencer Fox <spencer.fox85@gmail.com>
2016-04-29 11:42:11 -0500

    fixing issues

--- zika_code/TriggerScript.R
+++ zika_code/TriggerScript.R
@@ -9,14 +9,13 @@ if(grepl('laurencastro', Sys.info()['login'])) setwd('~/Documents/zika_alarm/zik
 
 sapply(c('branch.functions.R','plot.functions.R', 'analyze_saved_sims.R'), source)
 library(plyr)
+library(ggplot2)
 library(maptools)
+library(ggplot2)
 library(rgeos)
 library(rgdal)
 library(raster)
 library(plyr)
-library(cowplot)
-
-fig_path <- "~/Documents/zika_alarm/ExploratoryFigures/"
 
 #### Read in file with county R0 and Texas shape file
 county_plot <- read.csv("../csvs/county_plot.csv")
@@ -35,12 +34,18 @@ triggers <- get_trigger_data(rnots, intro = import.rates,
                              disc = 0.0224, threshold = 20, confidence = .8)
 
 #### Match R0/Import Rate with Trigger
-county_plot.m <- merge(x = county_plot.m, y = triggers[,c("r_not", "intro_rate", "prev_trigger")], 
-              by.x=c("R0_round", "import.rate"), by.y=c("r_not", "intro_rate"), all.x=TRUE, sort=FALSE)
+
+county_plot.m$trigger <- NA
+for (i in 1:nrow(triggers)) {
+  indices = which((county_plot.m$metro_round == triggers[i,"r_not"]) & 
+                    (county_plot.m$import.rate== triggers[i, "intro_rate"])) 
+  county_plot.m$trigger[indices] = triggers[i,"prev_trigger"]
+}
+county_plot.m$trigger <- as.numeric(county_plot.m$trigger)
+
 
 #### Fortifying Data to ShapeFile for ggplot 
 texas.county.f <- fortify(texas.county, region = "ID")
-texas.county.f$id <- as.numeric(texas.county.f$id)
 merge.texas.county <- merge(texas.county.f, county_plot.m, by = "id", all.x = TRUE)
 final.plot <- merge.texas.county[order(merge.texas.county$id),]
 final.plot$import.type <- factor(final.plot$scenario, 
@@ -50,27 +55,28 @@ final.plot$import.type <- factor(final.plot$scenario,
 
 
 #Actual plotting 
-  theme_cowplot() %+replace% theme(legend.position = "left", legend.key.size =  unit(0.5, "in") )+
-  labs(x=NULL, y = NULL) 
-print(plot.importation)
+library(cowplot)
+
 ###### Trigger Maps, Faceted by Scenario 
-breaks.trigger = seq(from = 0, to = max(final.plot$prev_trigger,na.rm=TRUE), by = 10)
+breaks.trigger = seq(from = 0, to = max(final.plot$trigger,na.rm=TRUE), by = 10)
 plot.trial <- ggplot(final.plot, aes(x=long, y = lat)) +
-  geom_polygon(data = final.plot, aes(group = group, fill = prev_trigger), color = "black", size = .25) +
+  geom_polygon(data = final.plot, aes(group = group, fill = trigger), color = "black", size = .25) +
   facet_wrap(~scenario, nrow = 2, dir = "h") +
   scale_x_continuous("", breaks=NULL) + 
   scale_y_continuous("", breaks=NULL) + 
-  scale_fill_continuous(name = "Surveillance \n Trigger \n", low = "red", high = "grey", 
+  scale_fill_continuous(name = "Surveillance \n Trigger \n", low = "red", high = "yellow", 
                         na.value = "grey", breaks = breaks.trigger) +
-  theme_cowplot() %+replace% theme(strip.background=element_blank(),
-                                   strip.text.x = element_blank(), 
-                                   legend.key.size =  unit(0.5, "in") )+
-  labs(x=NULL, y = NULL) 
-  
-print(plot.trial)
+  #theme_cowplot() %+replace% theme(strip.background=element_blank(),strip.text.x = element_blank()) +
+  theme_bw() + 
+  theme(axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank(), line = element_blank()) +
+  labs(x=NULL, y = NULL) +
+  theme(legend.position = "left") +
+  theme(legend.text=element_text(size=12, margin = margin(), debug = FALSE), legend.title = element_text(size = 18)) +
+  theme(legend.key.size =  unit(0.5, "in")) 
 
+ggsave(filename = "../ExploratoryFigures/figure3_triggers.pdf", plot = plot.trial) #, base_height = 4, base_aspect_ratio = 3)
+#ggsave(plot.trial, filename = "importation2.pdf", height = 7, width = 8, units = "in")
 
-save_plot(plot.trial, filename = "importation2.pdf", height = 7, width = 8, units = "in")
 
 #### Importation Probability Map 
 merge.texas.county.import <- merge(texas.county.f, county_plot, by = "id", all.x = TRUE)
@@ -95,7 +101,7 @@ plot.importation <- ggplot(import.plot, aes(x = long, y = lat)) +
         legend.title = element_text(size = 20)) +
   theme(legend.key.size =  unit(0.5, "in")) 
 plot.importation
-ggsave(filename = paste0(fig_path, "figure3_importaton.pdf"), plot = plot.importation,
+ggsave(filename = "../ExploratoryFigures/figure3_importaton.pdf", plot = plot.importation,
        height = 5, width = 5.5, units = "in") #, base_height = 4, base_aspect_ratio = 3)
 
 
